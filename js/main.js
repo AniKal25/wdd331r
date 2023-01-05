23a48ceb9f62c882af7f23e8449d609328b24eec
AniKal25 <kal21006@byui.edu>
2023-01-05 21:37:09 +0100

    W01 Practice: Review Webpage

---
+++ js/main.js
@@ -0,0 +1,97 @@
+const links = [
+    {
+      label: "Week 1 Practice: Review Webpage",
+      url: "week1/index.html"
+      
+    },
+
+    {
+      label: "Week 2",
+      url: "week2/index.html"
+      
+    },
+
+    {
+      label: "Week 3",
+      url: "week3/index.html"
+      
+    },
+
+    {
+      label: "Week 4",
+      url: "week4/index.html"
+      
+    },
+
+    {
+      label: "Week 5",
+      url: "week5/index.html"
+      
+    },
+
+    {
+      label: "Week 6",
+      url: "week6/index.html"
+      
+    },
+
+    {
+      label: "Week 7",
+      url: "week7/index.html"
+      
+    },
+
+    {
+      label: "Week 8",
+      url: "week8/index.html"
+      
+    },
+
+    {
+      label: "Week 9",
+      url: "week9/index.html"
+      
+    },
+
+    {
+      label: "Week 10",
+      url: "week10/index.html"
+      
+    },
+
+    {
+      label: "Week 11",
+      url: "week11/index.html"
+      
+    },
+
+    {
+      label: "Week 12",
+      url: "week12/index.html"
+      
+    },
+
+    {
+      label: "Week 13",
+      url: "week13/index.html"
+      
+    },
+
+  ]
+
+  let ol = document.getElementById("linksList");
+
+
+
+for (let i=0; i < links.length; i++) {
+    console.log(links)
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+
+    a.textContent = links[i].label;
+    a.setAttribute ("href", links[i].url)
+
+    li.append(a);
+    ol.append(li);
+   
+};
---
+++ js/script.js
@@ -0,0 +1,3 @@
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+document.getElementById("theYear").textContent = myYear;
